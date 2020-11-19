ec58c623659bf85d7bf6c4d6508412ebd656487b
Your Name <you@example.com>
2020-11-19 18:09:56 -0500

    malo

--- js/principal.js
+++ js/principal.js
@@ -1,25 +1,38 @@
 $(function() {
 
-    $('#dialogEliminar').dialog({
-        autoOpen: false,
-        modal: true,
-        buttons: {
-
-            'Acepto': function() {
-                alert('eliminado');
-            }
-        },
-        Cancel: function() {
-            console.log('hola qe')
-        }
-
-    });
+            var celulares = [
+                'LG', 'Motorola', 'Xiaomi', 'Samsung'
+            ]
 
 
-    $('.btn-eliminar').on('click', function() {
 
-        $('#dialogEliminar').dialog('open');
+            for (let i = 0; i < celulares.length; i++) {
+                const cell = celulares[i];
+                console.log(cell);
+                $('#lista-celulares').append(´ < li > $ { cell } < /li>´);
+
+                }
+
+                $('#dialogEliminar').dialog({
+                    autoOpen: false,
+                    modal: true,
+                    buttons: {
+
+                        'Acepto': function() {
+                            alert('eliminado');
+                        }
+                    },
+                    Cancel: function() {
+                        console.log('hola qe')
+                    }
+
+                });
 
 
-    })
-})
+                $('.btn-eliminar').on('click', function() {
+
+                    $('#dialogEliminar').dialog('open');
+
+
+                })
+            })
