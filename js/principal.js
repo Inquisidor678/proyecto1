5c680fcdb82ff862192f1b7d9b699842a38f21a1
Your Name <you@example.com>
2020-11-19 19:03:11 -0500

    definitivo

--- js/principal.js
+++ js/principal.js
@@ -1,38 +1,51 @@
 $(function() {
 
-            var celulares = [
-                'LG', 'Motorola', 'Xiaomi', 'Samsung'
-            ]
+    var celulares = [
+        'LG', 'Motorola', 'Xiaomi', 'Samsung'
+    ]
 
 
 
-            for (let i = 0; i < celulares.length; i++) {
-                const cell = celulares[i];
-                console.log(cell);
-                $('#lista-celulares').append(´ < li > $ { cell } < /li>´);
+    for (let i = 0; i < celulares.length; i++) {
+        const cell = celulares[i];
+        console.log(cell);
+        $('#lista-celulares').append(`
+            <li>
+                <span class="row">
 
-                }
+                    <span class="col">
+                        ${cell}
+                    </span>
+                    <span class="col">
+                        <a href="#" class="btn-eliminar">eliminar</a>
+                    </span>
+                    
+                </span>
+               
+            </li>
+            
+        `);
 
-                $('#dialogEliminar').dialog({
-                    autoOpen: false,
-                    modal: true,
-                    buttons: {
+    }
 
-                        'Acepto': function() {
-                            alert('eliminado');
-                        }
-                    },
-                    Cancel: function() {
-                        console.log('hola qe')
-                    }
+    $('#dialogEliminar').dialog({
+        autoOpen: false,
+        modal: true,
+        buttons: {
 
-                });
+            'Acepto': function() {
+                alert('eliminado');
+            }
+        },
+        Cancel: function() {
+            console.log('hola qe')
+        }
+
+    });
+
+    $('#lista-celulares').on('click', '.btn-eliminar', function() {
+        $('#dialogEliminar').dialog('open')
+    })
 
 
-                $('.btn-eliminar').on('click', function() {
-
-                    $('#dialogEliminar').dialog('open');
-
-
-                })
-            })
+});
